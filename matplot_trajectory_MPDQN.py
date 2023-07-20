f8d8c67fa610d9ff0ae51a6e6691ff4c74dc727f
forealasting <p76101071@gs.ncku.edu.tw>
2023-07-20 18:45:30 +0800

    plot modify

--- matplot_trajectory_MPDQN.py
+++ matplot_trajectory_MPDQN.py
@@ -6,6 +6,7 @@ import warnings
 import os
 from matplotlib import MatplotlibDeprecationWarning
 import matplotlib.lines as mlines
+import matplotlib
 warnings.filterwarnings('ignore', category=MatplotlibDeprecationWarning)
 # delay modify = average every x delay (x = 10, 50, 100)
 # request rate r
@@ -21,7 +22,7 @@ if if_evaluation:
 # tmp_str = "result2/result_cpu" # result_1016/tm1
 #tmp_dir = "pdqn_result/result2"
 # tmp_dir = "offline/database4"
-tmp_dir = "mpdqn_result/result1/evaluate1/"
+tmp_dir = "mpdqn_result/result4/evaluate14/"
 path1 = tmp_dir + "/app_mn1_trajectory.txt"
 path2 = tmp_dir + "/app_mn2_trajectory.txt"
 
@@ -75,24 +76,25 @@ def parse(p):
     return parsed_data
     # return step, replica, cpu_utilization, cpus, reward, resource_use
 
-
+matplotlib.rcParams.update({'font.size': 15})
 # Plot --------------------------------------
-def fig_add_Cpus_Replicas(x, y1, y2, service_name):
-    fig, ax2 = plt.subplots(figsize=(12, 6))  # 建立一個 Figure 和第二個軸 ax2
+def fig_add_Cpus_Replicas(x, y2, y1, service_name):
+    fig, ax2 = plt.subplots(figsize=(10, 4))  # 建立一個 Figure 和第二個軸 ax2
     ax1 = ax2.twinx()  # 在同一個 Figure 上建立第一個軸 ax1 (共享 x 軸)
 
     line_replicas = ax2.plot(x, y1, color="green", linestyle="-.")[0]  # 在第二個軸上繪製綠色的 Replicas 圖形
     line_cpus = ax1.plot(x, y2, color="blue", linestyle="--")[0]  # 在第一個軸上繪製藍色的 Cpus 圖形
 
-    ax2.set_xlabel("step", fontsize=12)  # 設定 x 軸標籤
-    ax2.set_ylabel("Replicas", fontsize=12)  # 設定第二個軸的 y 軸標籤
-    ax1.set_ylabel("Cpus", fontsize=12)  # 設定第一個軸的 y 軸標籤
+    ax2.set_xlabel("step", )  # 設定 x 軸標籤
+    ax2.set_ylabel("Replicas", )  # 設定第二個軸的 y 軸標籤
+    ax1.set_ylabel("Cpus", )  # 設定第一個軸的 y 軸標籤
 
     ax2.set_xlim(0, total_episodes * step_per_episodes)  # 設定 x 軸範圍
-    ax2.set_ylim(0, 4)  # 設定第二個軸的 y 軸範圍
+    ax2.set_ylim(0, 3.5)  # 設定第二個軸的 y 軸範圍
     ax1.set_ylim(0, 1.1)  # 設定第一個軸的 y 軸範圍
 
     ax2.tick_params(axis="both", labelsize=15)
+    ax2.set_yticks([0, 1, 2, 3])
     ax1.tick_params(axis="both", labelsize=15)
 
     line_replicas_color = line_replicas.get_color()
@@ -104,14 +106,13 @@ def fig_add_Cpus_Replicas(x, y1, y2, service_name):
     replicas_legend = mlines.Line2D([], [], color=line_replicas_color, linestyle=line_replicas_style, label='Replicas')
     cpus_legend = mlines.Line2D([], [], color=line_cpus_color, linestyle=line_cpus_style, label='Cpus')
 
-    ax2.set_title(service_name, fontsize=12, pad=20)  # 設定圖的標題
+    ax2.set_title(service_name, pad=20)  # 設定圖的標題
     # 建立圖例
     handles = [replicas_legend, cpus_legend]
     labels = [handle.get_label() for handle in handles]
-    fig.legend(handles, labels, loc='upper right', bbox_to_anchor=(1, 1))
-
-    plt.savefig(tmp_dir + service_name + "_Combined.png", dpi=300)
+    fig.legend(handles, labels, loc='upper right', bbox_to_anchor=(1, 1), fontsize=12)
     plt.tight_layout()
+    plt.savefig(tmp_dir + service_name + "_Combined.png", dpi=300)
     plt.show()
 
 
@@ -120,14 +121,14 @@ def fig_add_Cpus(x, y, service_name):
     plt.plot(x, y, color="blue")  # color=color
     plt.title(service_name)
     #　plt.xlabel("step")
-    plt.xlabel("step", fontsize=12)
-    plt.ylabel("Cpus", fontsize=12)
+    plt.xlabel("step", )
+    plt.ylabel("Cpus", )
     # plt.grid(True)
 
     plt.xlim(0, total_episodes*step_per_episodes)
     plt.ylim(0, 1.1)
-    plt.xticks(fontsize=15)
-    plt.yticks(fontsize=15)
+    plt.xticks()
+    plt.yticks()
     plt.savefig(tmp_dir + service_name + "_Cpus.png", dpi=300)
     plt.tight_layout()
     plt.show()
@@ -137,20 +138,20 @@ def fig_add_Replicas(x, y, service_name):
     # plt.figure()
     plt.plot(x, y, color="green")  # color=color
     plt.title(service_name)
-    plt.xlabel("step", fontsize=12)
-    plt.ylabel("Replicas", fontsize=12)
+    plt.xlabel("step", )
+    plt.ylabel("Replicas", )
     # plt.grid(True)
     plt.xlim(0, total_episodes*step_per_episodes)
     plt.ylim(0, 4)
-    plt.xticks(fontsize=15)
-    plt.yticks(fontsize=15)
-    plt.savefig(tmp_dir + service_name + "_Replicas.png", dpi=300)
+    plt.xticks()
+    plt.yticks(ticks=[0, 1, 2, 3, 4])
     plt.tight_layout()
+    plt.savefig(tmp_dir + service_name + "_Replicas.png", dpi=300)
     plt.show()
 
 
 def fig_add_Cpu_utilization(x, y, y_, service_name):
-    plt.figure(figsize=(12, 6))
+    plt.figure(figsize=(10, 4))
     if not if_evaluation:
         plt.plot(x, y, color='royalblue', alpha=0.2)  # color=color # label=label
         plt.plot(x, y_, color='royalblue')  # color=color
@@ -161,20 +162,20 @@ def fig_add_Cpu_utilization(x, y, y_, service_name):
 
     avg = sum(y) / len(y)
     plt.title(service_name + " Avg : " + str(avg))
-    plt.xlabel("step", fontsize=12)
-    plt.ylabel("Cpu_utilization", fontsize=12)
+    plt.xlabel("step", )
+    plt.ylabel("Cpu utilization(%)", )
     # plt.grid(True)
     plt.xlim(0, total_episodes*step_per_episodes)
     plt.ylim(0, 110)
-    plt.xticks(fontsize=15)
-    plt.yticks(fontsize=15)
-    plt.savefig(tmp_dir + service_name + "_Cpu_utilization.png", dpi=300)
+    plt.xticks()
+    plt.yticks(ticks=[0, 25, 50, 75, 100], )
     plt.tight_layout()
+    plt.savefig(tmp_dir + service_name + "_Cpu_utilization.png", dpi=300)
     plt.show()
 
 
 def fig_add_response_times(x, y, y_, service_name):
-    plt.figure(figsize=(12, 6))
+    plt.figure(figsize=(10, 4))
     if not if_evaluation:
         plt.plot(x, y, color="purple", alpha=0.2)  # color=color # label=label
         plt.plot(x, y_, color="purple")  # color=color # label=label
@@ -184,9 +185,13 @@ def fig_add_response_times(x, y, y_, service_name):
     avg = sum(y) / len(y)
     median = statistics.median(y)
     print("median response time", median)
+    with open(tmp_dir + 'median.txt', 'w') as file:
+        file.write("median: " + str(median))
+
+
     plt.title(service_name + " Avg : " + str(avg))
-    plt.xlabel("step", fontsize=12)
-    plt.ylabel("Response time", fontsize=12)
+    plt.xlabel("step", )
+    plt.ylabel("Response time", )
     if service_name == "First_level_mn1":
         Rmax = Rmax_mn1
     else:
@@ -200,10 +205,11 @@ def fig_add_response_times(x, y, y_, service_name):
     plt.axhline(y=Rmax_mn1, color='r', linestyle='--')
     plt.xlim(0, total_episodes*step_per_episodes)
     plt.ylim(0, 50)
-    plt.xticks(fontsize=15)
-    plt.yticks(fontsize=15)
-    plt.savefig(tmp_dir + service_name + "_Response_time.png", dpi=300)
+    plt.xticks()
+    plt.yticks()
     plt.tight_layout()
+    plt.savefig(tmp_dir + service_name + "_Response_time.png", dpi=300)
+
     plt.show()
 
 
@@ -223,13 +229,13 @@ def fig_add_Resource_use(x, y, y_, service_name, dir):
     print(service_name + " Avg_Resource_use", avg)
 
     plt.title(service_name + " Avg : " + str(avg))
-    plt.xlabel("step", fontsize=12)
-    plt.ylabel("Resource_use", fontsize=12)
+    plt.xlabel("step", )
+    plt.ylabel("Resource_use", )
     # plt.grid(True)
     plt.xlim(0, total_episodes*step_per_episodes)
     plt.ylim(0, 3)
-    plt.xticks(fontsize=15)
-    plt.yticks(fontsize=15)
+    plt.xticks()
+    plt.yticks()
     plt.savefig(dir + service_name + "_Resource_use.png", dpi=300)
     plt.tight_layout()
     plt.show()
@@ -241,17 +247,17 @@ def fig_add_reward(x, y, y_, service_name):
     plt.plot(x, y, color="red", alpha=0.2)  # color=color # label=label
     plt.plot(x, y_, color="red")  # color=color # label=label
     avg = sum(y) / len(y)
-    plt.title(service_name + " Avg : " + str(avg))
+    plt.title(service_name + " Avg : " + str(avg), fontsize=12)
     plt.xlabel("step", fontsize=12)
     plt.ylabel("Reward", fontsize=12)
 
     # plt.grid(True)
     plt.xlim(0, total_episodes*step_per_episodes)
     plt.ylim(-0.6, 0)
-    plt.xticks(fontsize=15)
-    plt.yticks(fontsize=15)
-    plt.savefig(tmp_dir + service_name + "_cost.png", dpi=300)
+    plt.xticks(fontsize=12)
+    plt.yticks(fontsize=12)
     plt.tight_layout()
+    plt.savefig(tmp_dir + service_name + "_cost.png", dpi=300)
     plt.show()
 
 def moving_average(lst, move=5):
@@ -307,7 +313,10 @@ def parse_episods_data(episods_data, service_name):
     cpus_ = moving_average(cpus)
     reward_ = moving_average(reward)
     resource_use_ = moving_average(resource_use)
-    # cpu_utilization = [x + 20 for x in cpu_utilization]
+    # if service_name == 'app_mn1':
+    #     cpu_utilization = [x + 20 for x in cpu_utilization]
+    # else:
+    #     cpu_utilization = [x + 10 for x in cpu_utilization]
     fig_add_Cpus(step, cpus, service_name)
     fig_add_Replicas(step, replicas, service_name)
     fig_add_Cpus_Replicas(step, cpus, replicas, service_name)
