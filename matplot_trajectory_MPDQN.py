e78c081b5d2864c1f9f7e871c40daf29d5f4b22a
forealasting <p76101071@gs.ncku.edu.tw>
2023-07-22 17:38:18 +0800

    plot modify

--- matplot_trajectory_MPDQN.py
+++ matplot_trajectory_MPDQN.py
@@ -78,24 +78,25 @@ def parse(p):
 
 matplotlib.rcParams.update({'font.size': 15})
 # Plot --------------------------------------
-def fig_add_Cpus_Replicas(x, y2, y1, service_name):
-    fig, ax2 = plt.subplots(figsize=(10, 4))  # 建立一個 Figure 和第二個軸 ax2
+def fig_combined_all(x, y2, y1, y3, y4, service_name):
+    # Create a figure with two subplots
+    fig, (ax4, ax3, ax2) = plt.subplots(3, 1, figsize=(10, 12), sharex=True)
+
     ax1 = ax2.twinx()  # 在同一個 Figure 上建立第一個軸 ax1 (共享 x 軸)
 
     line_replicas = ax2.plot(x, y1, color="green", linestyle="-.")[0]  # 在第二個軸上繪製綠色的 Replicas 圖形
     line_cpus = ax1.plot(x, y2, color="blue", linestyle="--")[0]  # 在第一個軸上繪製藍色的 Cpus 圖形
 
     ax2.set_xlabel("step", )  # 設定 x 軸標籤
-    ax2.set_ylabel("Replicas", )  # 設定第二個軸的 y 軸標籤
-    ax1.set_ylabel("Cpus", )  # 設定第一個軸的 y 軸標籤
+    ax2.set_ylabel("Replicas", labelpad=30)  # 設定第二個軸的 y 軸標籤
+    ax1.set_ylabel("Cpus",)  # 設定第一個軸的 y 軸標籤
 
     ax2.set_xlim(0, total_episodes * step_per_episodes)  # 設定 x 軸範圍
     ax2.set_ylim(0, 3.5)  # 設定第二個軸的 y 軸範圍
     ax1.set_ylim(0, 1.1)  # 設定第一個軸的 y 軸範圍
-
-    ax2.tick_params(axis="both", labelsize=15)
+    ax1.set_yticks([0, 0.2, 0.4, 0.6, 0.8, 1.0])
     ax2.set_yticks([0, 1, 2, 3])
-    ax1.tick_params(axis="both", labelsize=15)
+
 
     line_replicas_color = line_replicas.get_color()
     line_replicas_style = line_replicas.get_linestyle()
@@ -105,6 +106,73 @@ def fig_add_Cpus_Replicas(x, y2, y1, service_name):
     # 建立線條圖示
     replicas_legend = mlines.Line2D([], [], color=line_replicas_color, linestyle=line_replicas_style, label='Replicas')
     cpus_legend = mlines.Line2D([], [], color=line_cpus_color, linestyle=line_cpus_style, label='Cpus')
+    # ax2.set_title(service_name, pad=20)
+    # Add legends to the first subplot (ax1)
+    ax2.legend(handles=[replicas_legend, cpus_legend], loc='upper center', fontsize=12, ncol=2, bbox_to_anchor=(0.5, 1.2))
+
+
+    # ------------------------------------------------
+    # Plot Cpu utilization on the second subplot (ax3)
+    ax3.plot(x, y3, color='royalblue')
+    ax3.fill_between(x, 0, y3, color='royalblue')
+    ax3.set_xlabel('Step')
+    ax3.set_ylabel('Cpu utilization(%)', labelpad=10)
+    ax3.set_ylim(0, 110)
+    ax3.set_yticks([0, 25, 50, 75, 100])
+
+    avg = statistics.mean(y3)
+    ax3.set_title(service_name + " Avg : " + str(avg), pad=20)
+    # -----------------------------------------------
+    # Plot additional data on the fourth subplot (ax4)
+    ax4.plot(x, y4, color='purple')
+    ax4.fill_between(x, 0, y4, color="purple")
+    ax4.set_ylabel("Response time(ms)", labelpad=20)
+    ax4.set_ylim(0, 50)  # Set y-axis range for the fourth subplot
+    ax4.set_yticks([0, 10, 20, 30, 40, 50])
+    ax4.set_xlabel('Step')
+
+    avg = statistics.mean(y4)
+    ax4.set_title(service_name + " Avg : " + str(avg), pad=20)
+    if service_name == "First_level_mn1":
+        Rmax = Rmax_mn1
+    else:
+        Rmax = Rmax_mn2
+    ax4.axhline(y=Rmax, color='r', linestyle='--')
+
+    plt.tight_layout()
+    plt.savefig(tmp_dir + service_name + "_Combined_all.png", dpi=300)
+    plt.show()
+
+
+
+def fig_add_Cpus_Replicas(x, y2, y1, service_name):
+
+    fig, ax2 = plt.subplots(figsize=(10, 4))  # 建立一個 Figure 和第二個軸 ax2
+    ax1 = ax2.twinx()  # 在同一個 Figure 上建立第一個軸 ax1 (共享 x 軸)
+
+    line_replicas = ax2.plot(x, y1, color="green", linestyle="-.")[0]  # 在第二個軸上繪製綠色的 Replicas 圖形
+    line_cpus = ax1.plot(x, y2, color="blue", linestyle="--")[0]  # 在第一個軸上繪製藍色的 Cpus 圖形
+
+    ax2.set_xlabel("step", )  # 設定 x 軸標籤
+    ax2.set_ylabel("Replicas", )  # 設定第二個軸的 y 軸標籤   labelpad=30
+    ax1.set_ylabel("Cpus",)  # 設定第一個軸的 y 軸標籤
+
+    ax2.set_xlim(0, total_episodes * step_per_episodes)  # 設定 x 軸範圍
+    ax2.set_ylim(0, 3.5)  # 設定第二個軸的 y 軸範圍
+    ax1.set_ylim(0, 1.1)  # 設定第一個軸的 y 軸範圍
+    ax2.set_yticks([0, 1, 2, 3])
+    # ax2.tick_params(axis="both", labelsize=15)
+    # ax1.tick_params(axis="both", labelsize=15)
+
+    line_replicas_color = line_replicas.get_color()
+    line_replicas_style = line_replicas.get_linestyle()
+    line_cpus_color = line_cpus.get_color()
+    line_cpus_style = line_cpus.get_linestyle()
+
+    # 建立線條圖示
+    replicas_legend = mlines.Line2D([], [], color=line_replicas_color, linestyle=line_replicas_style, label='Replicas')
+    cpus_legend = mlines.Line2D([], [], color=line_cpus_color, linestyle=line_cpus_style, label='Cpus')
+
 
     ax2.set_title(service_name, pad=20)  # 設定圖的標題
     # 建立圖例
@@ -117,14 +185,15 @@ def fig_add_Cpus_Replicas(x, y2, y1, service_name):
 
 
 def fig_add_Cpus(x, y, service_name):
-    # plt.figure()
+
     plt.plot(x, y, color="blue")  # color=color
     plt.title(service_name)
     #　plt.xlabel("step")
     plt.xlabel("step", )
     plt.ylabel("Cpus", )
     # plt.grid(True)
-
+    mean_cpus = statistics.mean(y)
+    print(service_name + "_avg_cpus:" + str(mean_cpus))
     plt.xlim(0, total_episodes*step_per_episodes)
     plt.ylim(0, 1.1)
     plt.xticks()
@@ -141,6 +210,8 @@ def fig_add_Replicas(x, y, service_name):
     plt.xlabel("step", )
     plt.ylabel("Replicas", )
     # plt.grid(True)
+    mean_Replicas = statistics.mean(y)
+    print(service_name + "_avg_Replicas:" + str(mean_Replicas))
     plt.xlim(0, total_episodes*step_per_episodes)
     plt.ylim(0, 4)
     plt.xticks()
@@ -186,12 +257,12 @@ def fig_add_response_times(x, y, y_, service_name):
     median = statistics.median(y)
     print("median response time", median)
     with open(tmp_dir + 'median.txt', 'a') as file:
-        file.write(service_name + "_median: " + str(median) + "/n")
+        file.write(service_name + "_median: " + str(median) + "\n")
 
 
     plt.title(service_name + " Avg : " + str(avg))
     plt.xlabel("step", )
-    plt.ylabel("Response time", )
+    plt.ylabel("Response time(ms)", )
     if service_name == "First_level_mn1":
         Rmax = Rmax_mn1
     else:
@@ -209,13 +280,11 @@ def fig_add_response_times(x, y, y_, service_name):
     plt.yticks()
     plt.tight_layout()
     plt.savefig(tmp_dir + service_name + "_Response_time.png", dpi=300)
-
     plt.show()
 
 
 def fig_add_Resource_use(x, y, y_, service_name, dir):
     plt.figure()
-    x = [i for i in range(len(y))]
 
     if not if_evaluation:
         plt.plot(x, y, color="black", alpha=0.2)  # color=color # label=label
@@ -271,6 +340,15 @@ def moving_average(lst, move=5):
 
     return moving_averages
 
+def store_trajectory(service_name, step, s, a_r, a_c, r, r_perf, r_res, s_, done, if_epsilon):
+    path = tmp_dir + service_name + "_trajectory123.txt"
+    tmp_s = list(s)
+    tmp_s_ = list(s_)
+    a_c_ = list(a_c)
+    f = open(path, 'a')
+    data = str(step) + ' ' + str(tmp_s) + ' ' + str(a_r) + ' ' + str(a_c_) + ' ' + str(r) + ' ' + str(r_perf) + ' ' + str(r_res) + ' ' + str(tmp_s_) + ' ' + str(done) + ' ' + str(if_epsilon) + '\n'
+    f.write(data)
+
 
 def parse_episods_data(episods_data, service_name):
     plot_name = ["replica", "cpu_utilization", "cpus", "reward", "resource_use"]
@@ -307,6 +385,8 @@ def parse_episods_data(episods_data, service_name):
                 response_times.append(parsed_line[7][3])
         # episode_reward.append(sum(reward)/len(reward))
     resource_use = [x * y for x, y in zip(replicas, cpus)]
+    print(statistics.mean(replicas)*statistics.mean(cpus))
+    print(statistics.mean(resource_use))
     replicas_ = moving_average(replicas)
     response_times_ = moving_average(response_times)
     cpu_utilization_ = moving_average(cpu_utilization)
@@ -317,28 +397,24 @@ def parse_episods_data(episods_data, service_name):
     #     cpu_utilization = [x + 20 for x in cpu_utilization]
     # else:
     #     cpu_utilization = [x + 10 for x in cpu_utilization]
-    fig_add_Cpus(step, cpus, service_name)
-    fig_add_Replicas(step, replicas, service_name)
-    fig_add_Cpus_Replicas(step, cpus, replicas, service_name)
-    fig_add_response_times(step, response_times, response_times_, service_name)
-    fig_add_Cpu_utilization(step, cpu_utilization, cpu_utilization_, service_name)
-    fig_add_Resource_use(step, resource_use, resource_use_, service_name, tmp_dir)
-    fig_add_reward(step, reward, reward_, service_name)
+    # fig_add_Cpus(step, cpus, service_name)
+    # fig_add_Replicas(step, replicas, service_name)
+    # fig_add_Cpus_Replicas(step, cpus, replicas, service_name)
+    # fig_add_response_times(step, response_times, response_times_, service_name)
+    # fig_add_Cpu_utilization(step, cpu_utilization, cpu_utilization_, service_name)
+    # fig_add_Resource_use(step, resource_use, resource_use_, service_name, tmp_dir)
+    # fig_add_reward(step, reward, reward_, service_name)
+    fig_combined_all(step, cpus, replicas, cpu_utilization, response_times, service_name)
+
 
 
 tmp_count = 0
 for p in path_list:
-
     episods_data = parse(p)
-    # print(len(episods_data), episods_data)
-    # step, replica, cpu_utilization, cpus, reward, resource_use = parse_episods_data(episods_data)
-    # print(len(episods_data))
     parse_episods_data(episods_data, service[tmp_count])
-
     tmp_count += 1
 
-#
-#
+
 # path_1 = tmp_dir + "/app_mn1_response.txt"
 # path_2 = tmp_dir + "/app_mn2_response.txt"
 # path_list = [path_1, path_2]
